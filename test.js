7b9743e90d7980c5c2a97d3ec34846eeaccc9454
FelixRilling <contact@f-rilling.com>
2017-03-30 22:31:21 +0200

    recursive approach

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const util = require("util");
 const Na = require("./index");
 
-const tag = fs.readFileSync("tags/pizzamaker.yna", {
+const tag = fs.readFileSync("tags/simple.yna", {
     encoding: "utf8"
 });
 const instance = new Na(tag);
