525adb08c40ab4dbc2e5aa6c91d47a9b3caad1d9
FelixRilling <contact@f-rilling.com>
2017-04-04 22:08:13 +0200

    basic optimizer

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 "use strict";
 
-const parseNa = require("./lib/parseNa");
-const runNa = require("./lib/runNa");
+const parse = require("./lib/parser/index");
+const optimize = require("./lib/optimizer/index");
 
 module.exports = class {
     constructor(na) {
         const _this = this;
+        const tree = parse(na);
+        const optimized = optimize(tree);
 
         _this.na = na;
-        _this.tree = parseNa(na);
+        _this.tree = tree;
+        _this.optimized = optimized;
     }
     run(args) {
-        return runNa(this.tree, args);
+        //return runNa(this.optimized, args);
     }
 };
