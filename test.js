7fb8291933215716fadd038492796c3c02812b52
FelixRilling <contact@f-rilling.com>
2017-04-07 12:22:57 +0200

    base map init

--- test.js
+++ test.js
@@ -8,7 +8,19 @@ const tag = fs.readFileSync("tags/simple.yna", {
     encoding: "utf8"
 });
 const instance = new Na(tag);
-const result = instance.run("foo");
+const result = instance.run("foo", {
+    tag: "tag",
+    meid: "meid",
+    caller: "caller",
+    callerid: "callerid",
+    channel: "channel",
+    channelid: "channelid",
+    server: "server",
+    serverid: "serverid",
+    time: Date.now(),
+    uses: "uses",
+    newrep: "false"
+});
 
 
-console.log(result)
+console.log(result);
