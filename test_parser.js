a59c1b8529ac69d93704f025a6ec50e4865a2021
FelixRilling <contact@f-rilling.com>
2017-04-09 20:27:35 +0200

    updated name

--- test_parser.js
+++ test_parser.js
@@ -3,14 +3,26 @@
 const fs = require("fs");
 const util = require("util");
 const Na = require("./index");
-
+const exampleData = {
+    user: {
+        name: "NobodyRocks",
+        id: "128985967875850240",
+        discriminator: "5051",
+        avatar: "35a8b84d61abf268e326de42e90f33a7",
+        bot: false,
+        avatar_url: null,
+        default_avatar: null,
+        default_avatar_url: null,
+        mention: "<@!128985967875850240>",
+        created_at: "2016-04-07 15:23:01.000000",
+        display_name: "Nobeon"
+    }
+}
+	
 //Tag setup
-const tag_content = fs.readFileSync("tags/simple.yna", {
+const tag_content = fs.readFileSync("yna/simple.yna", {
     encoding: "utf8"
 });
-const instance = new Na("simple", tag_content, {
-    name: "​NobodyRocks#5051",
-    id: "273221196001181697"
-});
+const instance = new Na("simple", tag_content, exampleData);
 
 console.log(JSON.stringify(instance.tree," "," "));
