70936b7a9b22efb7130d26af2486a7acb1c7a4cd
Felix Rilling <contact@f-rilling.com>
2017-04-22 13:34:25 +0200

    added readme

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # YNA
 
+A YNA Engine in JS
+
+[Spec](./SPEC.md)
+
+## Usage
+
+Tag instance creation:
+
+```js
+const Yna = require("ynajs");
+//Yna(yna, info, options)
+const tag = new Yna("{upper:arg1;}");
+```
+
+Tag execution:
+
+```js
+tag.run(["Argument1"]); //=> "ARGUMENT1"
+```
--- test_parser.js
+++ test_parser.js
@@ -3,12 +3,11 @@
 const fs = require("fs");
 const util = require("util");
 const Yna = require("./index");
-const content=[{"type":"command","name":"oneline","data":[["\n\t",{"type":"command","name":"set","data":["moji_hand",[{"type":"command","name":"choose","data":["/","\\\\","\\_/","\\\\\\_","\\_","7","<","=",">","\\~","?","�","�","n","+","+","?","?","?","?"]}]]},"\n\t",{"type":"command","name":"set","data":["moji_eye",[{"type":"command","name":"choose","data":["n","u","o","v","O","U","V","#","$","'","\\*","+","-",".","0","<","=",">","@","^","~","�","�","�","�","�","�","�","�","?","O","o","O","o","^","?","�","�","?","?","?","?","?","?","?","?","?","?","?","?","?","?","�","�","�","/�/","?","?","?","?","?","?","?","?","?","�","?","o","?","/>/","/</","?","?","?","?"]}]]},"\n\t",{"type":"command","name":"set","data":["moji_mouth",[{"type":"command","name":"choose","data":["#","+",".","6","<","=","-",">","O","U","V","W","\\_","o","u","v","w","~","�","?","?","O","e","?","?","?","?","?","�","�","�","�","?","?","?","?","?","?","?","O","?"]}]]},"\n\n\t",{"type":"key","name":"moji_hand"},"(",{"type":"key","name":"moji_eye"},{"type":"key","name":"moji_mouth"},{"type":"key","name":"moji_eye"},")",{"type":"key","name":"moji_hand"},"\n    ","\n"]]},"\n"];
-//Tag setup
+
 const tag_content = fs.readFileSync("yna/simple.yna", {
     encoding: "utf8"
 });
-const tag = new Yna(content, {},
+const tag = new Yna(tag_content, {},
     {
         parser: {
             debug: true
