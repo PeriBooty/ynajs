39390e55e50e8107239335143a83b93ee392bfce
Felix Rilling <contact@f-rilling.com>
2017-08-17 13:54:44 +0200

    added discord commands

--- test_runner.js
+++ test_runner.js
@@ -4,17 +4,21 @@ const Yna = require("./index");
 const tag_data = require("./test_data");
 
 //Data dump
-const tag = new Yna(tag_data);
+const tag = new Yna(tag_data, {
+    plugins: {
+        discord: true
+    }
+});
 let result;
 
-result = tag.run(["Foo","Bar"], {
+result = tag.run(["Foo", "Bar"], {
     foo: 1,
     bar: {
         __default: 1,
         bizz: 12
     }
 }, {
-    debug: true
+    debug: false,
 });
 
 console.log(result);
