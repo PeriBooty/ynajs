2acce3022ded3d9e83f37c9bdaabf831adb3b30a
Felix Rilling <contact@f-rilling.com>
2017-10-04 21:10:43 +0200

    refactured option/data set

--- index.js
+++ index.js
@@ -4,6 +4,33 @@ const parse = require("./lib/parser");
 const run = require("./lib/runner");
 const initCommands = require("./lib/init/initCommands");
 const initKeys = require("./lib/init/initKeys");
+const {
+    defaultsDeep
+} = require("lodash");
+
+const optionsDefault = {
+    debug: false,
+    loadJSON: false,
+    plugins: {
+        init: true,
+        discord: false
+    }
+};
+const optionsRunnerDefault = {
+    debug: false,
+    depth: 0
+};
+const dataDefault = {
+    discord: {
+        msg: null, //Discord.js message collection
+        app: null, //Di-ngy app instance
+        storing: {
+            tag: null,
+            tagStorage: null,
+            tagKey: null,
+        }
+    }
+};
 
 /**
  * YNA command class
@@ -17,9 +44,12 @@ module.exports = class {
      * @param {string} yna
      * @param {Object} [options={}]
      */
-    constructor(yna, options = {}) {
-        this.tree = parse(yna, options);
-        this.commandMap = initCommands(options);
+    constructor(yna, options = {}, data = {}) {
+        const optionsMerged = defaultsDeep(options, optionsDefault);
+        const dataMerged = defaultsDeep(data, dataDefault);
+
+        this.tree = parse(yna, optionsMerged, dataMerged);
+        this.commandMap = initCommands(optionsMerged);
     }
     /**
      * Adds a new command to the instance container
@@ -38,9 +68,11 @@ module.exports = class {
      * @param {Object} [options={}]
      * @returns {string}
      */
-    run(args = [], ctx = {}, options = {}) {
+    run(args = [], ctx = {}, options = {}, data = {}) {
+        const optionsMerged = defaultsDeep(options, optionsRunnerDefault);
+        const dataMerged = defaultsDeep(data, dataDefault);
         const keyMap = initKeys(args, ctx);
 
-        return run(this.tree, this.commandMap, keyMap, options);
+        return run(this.tree, this.commandMap, keyMap, optionsMerged, dataMerged);
     }
 };
